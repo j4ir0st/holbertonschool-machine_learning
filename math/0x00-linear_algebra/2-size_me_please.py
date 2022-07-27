705e6c0c1ac6671c33fd5b970b26a877d06f9729
j4ir0st <jairocast2@gmail.com>
2022-07-27 18:26:22 -0500

    Fixing documentation

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+""" Shape """
 import numpy
 
 
 def matrix_shape(matrix):
+    """ Return shape as list """
     a = numpy.array(matrix)
     li = list(a.shape)
     return li
