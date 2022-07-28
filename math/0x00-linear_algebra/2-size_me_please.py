0517dda7bde3a22696e64b62464af2b670a947ce
j4ir0st <jairocast2@gmail.com>
2022-07-27 23:05:08 -0500

    Making logic from start - while conditional

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -5,18 +5,7 @@
 def matrix_shape(matrix):
     """ Return shape as list """
     count = []
-    c1 = 0
-    for elem in matrix:
-        c1 = c1 + 1
-        c2 = 0
-        for el in elem:
-            c2 = c2 + 1
-            c3 = 0
-        if type(el) != int:
-            for e in el:
-                c3 = c3 + 1
-    count.append(c1)
-    count.append(c2)
-    if type(el) != int:
-        count.append(c3)
+    while(type(matrix) is list):
+        count.append(len(matrix))
+        matrix = matrix[0]
     return count
