d019fdfb22a60847986112687928f63bca39495e
j4ir0st <jairocast2@gmail.com>
2022-07-28 20:53:36 -0500

    concat 2 matruces

---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" concatenates two arrays """
+
+
+def cat_arrays(arr1, arr2):
+    """ concatenates two arrays """
+    cat = []
+    for a1 in arr1:
+        cat.append(a1)
+    for a2 in arr2:
+        cat.append(a2)
+    return cat
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """ concatenates two matrices """
+    cat = []
+    if axis == 0:
+        m = cat_arrays(mat1, mat2)
+        cat.append(m)
+        return cat
+    else:
+        for e in range(len(mat1)):
+           arr = cat_arrays(mat1[e], mat2[e])
+           cat.append(arr)
+    return cat
