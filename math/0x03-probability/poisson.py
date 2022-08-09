abbe9b0fa299843c374468c160e26dcd6cbe9fd5
j4ir0st <jairocast2@gmail.com>
2022-08-08 20:04:45 -0500

    calculates the lambtha of the data

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Creating Poisson class
 """
@@ -7,4 +7,17 @@ Creating Poisson class
 class Poisson:
     """ Represents a poisson distribution """
     def __init__(self, data=None, lambtha=1.):
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError('lambtha must be a positive value')
+            self.lambtha = float(lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError('data must be a list')
+            acum = 0
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            for elem in data:
+                acum += elem
+            self.lambtha = float(acum / len(data))
         
