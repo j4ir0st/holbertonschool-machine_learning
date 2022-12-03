051fa40935db8df7d861bf159153a3386821b01d
j4ir0st <jairocast2@gmail.com>
2022-12-03 00:16:35 -0500

    performs matrix multiplication

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -2,22 +2,17 @@
 """ concatenates two arrays """
 
 
-def cat_arrays(arr1, arr2):
-    """ concatenates two arrays """
-    cat = []
-    for a1 in arr1:
-        cat.append(a1)
-    for a2 in arr2:
-        cat.append(a2)
-    return cat
-
-
 def mat_mul(mat1, mat2):
     """ matrix multiplication """
     mul = []
-    aux = []
-    if len(mat1) != len(mat2[0]):
+    if len(mat2) != len(mat1[0]):
         return None
-    for i in len(mat1):
-        for j in len(mat2[0]):
-            aux.append(mat1[i][j] * mat2[0][0])
+    for i in range(len(mat1)):
+        aux = []
+        for j in range(len(mat2[0])):
+            a = 0
+            for k in range(len(mat2)):
+                a += mat1[i][k] * mat2[k][j]
+            aux.append(a)
+        mul.append(aux)
+    return(mul)
