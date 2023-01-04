b4af1b1b8c24cbda8a5f69aeb44cc5e507faf8ec
j4ir0st <jairocast2@gmail.com>
2023-01-03 22:40:11 -0500

    Neuron Class: Removing nx assignment

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -13,7 +13,6 @@ class Neuron:
             raise TypeError('nx must be an integer')
         if (nx < 1):
             raise ValueError('nx must be a positive integer')
-        self.nx = nx
         self.W = np.random.randn(1, 784)
         self.b = 0
         self.A = 0
