8ba98f077c41a46b2c5a6034e1be8d316c46b34f
j4ir0st <jairocast2@gmail.com>
2023-01-04 00:37:30 -0500

    Neuron Class: Fixing identation 2

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -11,8 +11,8 @@ class Neuron:
     def __init__(self, nx):
         if not (isinstance(nx, int)):
             raise TypeError('nx must be an integer')
-            if (nx < 1):
-                raise ValueError('nx must be a positive integer')
+        if (nx < 1):
+            raise ValueError('nx must be a positive integer')
         self.W = np.random.randn(1, 784)
         self.b = 0
         self.A = 0
