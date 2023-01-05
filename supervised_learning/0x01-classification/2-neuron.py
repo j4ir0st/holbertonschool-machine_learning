df9e387ff9ac3feccd1bf1add1e93e55feb78ffa
j4ir0st <jairocast2@gmail.com>
2023-01-04 19:21:28 -0500

    Neuron Class: Adding sigmoid function

---
+++ supervised_learning/0x01-classification/2-neuron.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Creating a Neuron Class
+"""
+import numpy as np
+
+
+class Neuron:
+    """ Neuron Class: nx is the number of input features to the neuron """
+
+    def __init__(self, nx):
+        if not (isinstance(nx, int)):
+            raise TypeError('nx must be an integer')
+        if (nx < 1):
+            raise ValueError('nx must be a positive integer')
+        self.__nx = nx
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    def forward_prop(self, X):
+        a = sigmoid(np.dot(self.__W, X) + self.__b)
+        self.__A = a
+        return (a)
+
+    @property
+    def W(self):
+        return (self.__W)
+
+    @property
+    def b(self):
+        return (self.__b)
+
+    @property
+    def A(self):
+        return (self.__A)
+
+
+def sigmoid(z):
+    return 1.0/(1.0+np.exp(-z))
