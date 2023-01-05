c9793e3f2e9af60925de4f996f7aa9fb9c6c025f
j4ir0st <jairocast2@gmail.com>
2023-01-04 19:00:16 -0500

    Neuron Class: Fixing Parameter

--- supervised_learning/0x01-classification/1-neuron.py
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -13,7 +13,7 @@ class Neuron:
             raise TypeError('nx must be an integer')
         if (nx < 1):
             raise ValueError('nx must be a positive integer')
-        self.__W = np.random.randn(1, 784)
+        self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
 
