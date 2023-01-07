214b02161cfe3a0806f9c8c15b1ea6bc8816aae7
j4ir0st <jaircast2@gmail.com>
2023-01-06 23:33:02 -0500

    PlaceHolders: returns two placeholders, x and y, for the neural network

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -3,6 +3,6 @@
 
 def create_placeholders(nx, classes):
     """ returns two placeholders, x and y, for the neural network """
-    x = tf.placeholder('float', shape=[None, nx])
-    y = tf.placeholder('float', shape=[None, classes])
+    x = tf.placeholder(name="x", dtype=tf.float32, shape=(None, nx))
+    y = tf.placeholder(name="y", dtype=tf.float32, shape=(None, classes))
     return x, y
