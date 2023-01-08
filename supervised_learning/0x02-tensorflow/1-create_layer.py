4064f35668ff0db23a2397b2864c684cfa58c60a
j4ir0st <jaircast2@gmail.com>
2023-01-08 00:10:29 -0500

    Fixing pycodestyle

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """ Layers """
 import tensorflow.compat.v1 as tf
-import tensorflow.keras as kr
 
 
 def create_layer(prev, n, activation):
-
-    weights = kr.initializers.VarianceScaling(mode='fan_avg')
-    layers = kr.layers.Dense(units=n,
+    """ Create Layers """
+    weights = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    layers = tf.keras.layers.Dense(units=n,
                              activation=activation,
                              kernel_initializer=weights,
                              name='layer')
