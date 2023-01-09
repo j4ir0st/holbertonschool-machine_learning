b7bc32ca42e98bdffe97cbc1019e6f4b9dd445be
j4ir0st <jaircast2@gmail.com>
2023-01-09 02:28:45 -0500

    fordward propagation: fixing pycodestyle

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -5,12 +5,13 @@ import tensorflow.compat.v1 as tf
 
 create_layer = __import__('1-create_layer').create_layer
 
+
 def forward_prop(x, layer_sizes=[], activations=[]):
 
     # Initialize the input layer with the input data
     layer = x
 
-    # Iterate over the layers and activations and create a fully connected layer
+    # Iterate over the layers and activations and create a fully connected lyer
     for i in range(len(layer_sizes)):
         layer = create_layer(layer, layer_sizes[i], activations[i])
 
