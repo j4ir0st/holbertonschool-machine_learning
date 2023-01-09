64c0cc7fa0d6d70868a87a5df8f39b384d4a2aac
j4ir0st <jaircast2@gmail.com>
2023-01-09 02:26:12 -0500

    fordward propagation

---
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Forward Propagation """
+import tensorflow.compat.v1 as tf
+
+
+create_layer = __import__('1-create_layer').create_layer
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+
+    # Initialize the input layer with the input data
+    layer = x
+
+    # Iterate over the layers and activations and create a fully connected layer
+    for i in range(len(layer_sizes)):
+        layer = create_layer(layer, layer_sizes[i], activations[i])
+
+    # Return the output of the final layer
+    return layer
