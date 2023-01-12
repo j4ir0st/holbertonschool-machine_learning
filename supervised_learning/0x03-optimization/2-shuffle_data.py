3ac200e87ffeaabc8736c3bc8d7453cdaa5f5b95
j4ir0st <jaircast2@gmail.com>
2023-01-12 18:02:42 -0500

    Shuffle Data: Changing Method

--- supervised_learning/0x03-optimization/2-shuffle_data.py
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -6,9 +6,8 @@ import numpy as np
 
 def shuffle_data(X, Y):
     """ shuffles the data points in two matrices the same way """
+    x = X[np.random.permutation(len(X))]
     np.random.seed(0)
-    x = np.random.permutation(X)
-    np.random.seed(0)
-    y = np.random.permutation(Y)
+    y = Y[np.random.permutation(len(Y))]
 
     return x, y
