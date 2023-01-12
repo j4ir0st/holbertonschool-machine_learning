c635e0f8e26161003542b5680d6b5c17ff045fc2
j4ir0st <jaircast2@gmail.com>
2023-01-12 17:46:45 -0500

    Normalization Constants: Fixing pycodestyle

--- supervised_learning/0x03-optimization/0-norm_constants.py
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def normalization_constants(X):
-    """ calculates the normalization (standardization) constants of a matrix """
+    """ calculates the normalization constants of a matrix """
     u = np.average(X, axis=0)
     s = np.std(X, axis=0)
 
