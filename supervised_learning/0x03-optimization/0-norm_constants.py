3e85c2155a9c9f62a96635c995b87f90cf2c1c09
j4ir0st <jaircast2@gmail.com>
2023-01-11 19:31:31 -0500

    normalizes (standardizes) a matrix

---
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Normalization Constants """
+
+import numpy as np
+
+
+def normalization_constants(X):
+    """ calculates the normalization (standardization) constants of a matrix """
+    u = np.average(X, axis=0)
+    s = np.std(X, axis=0)
+
+    return u, s
