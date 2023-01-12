1d056d26f51318af0b44d265030acebada8cbd18
j4ir0st <jaircast2@gmail.com>
2023-01-12 17:48:30 -0500

    Shuffle Data: Setting seed to 0

--- supervised_learning/0x03-optimization/2-shuffle_data.py
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -6,6 +6,7 @@ import numpy as np
 
 def shuffle_data(X, Y):
     """ shuffles the data points in two matrices the same way """
+    np.random.seed(0)
     x = np.random.permutation(X)
     np.random.seed(0)
     y = np.random.permutation(Y)
