dacb2c5e9d4f1526660b5725eda38c9ef1c14801
j4ir0st <jaircast2@gmail.com>
2023-01-11 20:06:03 -0500

    shuffles the data points in two matrices the same way

---
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Shuffle Data """
+
+import numpy as np
+
+
+def shuffle_data(X, Y):
+    """ shuffles the data points in two matrices the same way """
+    x = np.random.permutation(X)
+    np.random.seed(0)
+    y = np.random.permutation(Y)
+
+    return x, y
