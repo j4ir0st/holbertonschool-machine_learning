034deb8b189269b758d566c2175cfa4523a0aacd
j4ir0st <jaircast2@gmail.com>
2023-01-25 18:50:06 -0500

    Input: fixing pycodestyle

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -7,11 +7,11 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """ builds a neural network with the Keras library """
     inp = K.Input(shape=(nx, ))
     reg = K.regularizers.L2(l2=lambtha)
-    for i in range(0, len(layers)):
+    for i in range(len(layers)):
         if i == 0:
             out = K.layers.Dense(units=layers[0],
-                         activation=activations[0],
-                         kernel_regularizer=reg)(inp)
+                                 activation=activations[0],
+                                 kernel_regularizer=reg)(inp)
         else:
             out = K.layers.Dense(units=layers[i],
                                  activation=activations[i],
