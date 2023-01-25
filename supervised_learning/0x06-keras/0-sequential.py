398d8d2e6ad11138664135fff91ccafb5c79dd33
j4ir0st <jaircast2@gmail.com>
2023-01-25 07:40:10 -0500

    Sequential: Fixing for function

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -9,13 +9,13 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     reg = K.regularizers.L2(l2=lambtha)
     for i in range(len(layers)):
         if i == 0:
-            model.add(K.layers.Dense(units=layers[0],
-                                     activation=activations[0],
+            model.add(K.layers.Dense(units=layers[i],
+                                     activation=activations[i],
                                      kernel_regularizer=reg,
                                      input_shape=(nx, )))
         else:
-            model.add(K.layers.Dense(units=layers[1],
-                                     activation=activations[1],
+            model.add(K.layers.Dense(units=layers[i],
+                                     activation=activations[i],
                                      kernel_regularizer=reg))
         if i < len(layers) - 1:
             model.add(K.layers.Dropout(rate=(1 - keep_prob)))
