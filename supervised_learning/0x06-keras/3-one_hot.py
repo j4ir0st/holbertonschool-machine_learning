f5a45b690c64bf70cc5686e4952194759541b6f8
j4ir0st <jaircast2@gmail.com>
2023-01-25 20:29:16 -0500

    One Hot: adding number of classes

--- supervised_learning/0x06-keras/3-one_hot.py
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -5,6 +5,5 @@ import tensorflow.keras as K
 
 def one_hot(labels, classes=None):
     """ converts a label vector into a one-hot matrix """
-    size = len(labels)
-    cat = K.utils.to_categorical(labels)
+    cat = K.utils.to_categorical(labels, num_classes=classes)
     return cat
