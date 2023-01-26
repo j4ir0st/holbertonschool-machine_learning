7f2740176c7e3e0752c0da570bd7b93dbcfe64e8
j4ir0st <jaircast2@gmail.com>
2023-01-25 19:49:41 -0500

    Optimize: Adding loss and metrics

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -9,5 +9,7 @@ def optimize_model(network, alpha, beta1, beta2):
     Adam = K.optimizers.Adam(learning_rate=alpha,
                              beta_1=beta1,
                              beta_2=beta2)
-    network.compile(optimizer=Adam)
+    network.compile(loss='categorical_crossentropy',
+                    metrics=["accuracy"],
+                    optimizer=Adam)
     return None
