2b32cab2acf8d954442ddd01d217839982273e85
j4ir0st <jaircast2@gmail.com>
2023-01-25 19:41:08 -0500

    Optimize: sets up Adam optimization for a keras model

---
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Optimize """
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """ sets up Adam optimization for a keras model
+        with categorical crossentropy loss and accuracy metrics """
+    Adam = K.optimizers.Adam(learning_rate=alpha,
+                             beta_1=beta1,
+                             beta_2=beta2)
+    network.compile(optimizer=Adam)
+    return None
