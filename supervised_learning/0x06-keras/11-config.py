3d3310e506066a8754ec5dcdce45b3f03213ba24
j4ir0st <jaircast2@gmail.com>
2023-01-27 16:55:51 -0500

    Save and Load Configuration

--- supervised_learning/0x06-keras/11-config.py
+++ supervised_learning/0x06-keras/11-config.py
@@ -7,11 +7,13 @@ def save_config(network, filename):
     """ saves a model’s configuration in JSON format """
     json_conf = network.to_json()
     with open(filename, 'w') as f:
-        j.write(json_conf)
+        f.write(json_conf)
     return None
 
 
 def load_config(filename):
     """ loads a model with a specific configuration """
-    load_w = K.models.model_from_json(filename)
+    with open(filename, 'r') as f:
+        model = f.read()
+    load_c = K.models.model_from_json(model)
     return load_w
