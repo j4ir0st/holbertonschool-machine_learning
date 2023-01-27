8b39891f1ae11721fe521862528b1bfd6e7c6906
j4ir0st <jaircast2@gmail.com>
2023-01-27 06:47:42 -0500

    Early Stopping: Fixing function

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -10,7 +10,7 @@ def train_model(network,
                 epochs,
                 validation_data=None,
                 early_stopping=False,
-                patience=0
+                patience=0,
                 verbose=True,
                 shuffle=False):
     """ trains a model using mini-batch gradient descent """
